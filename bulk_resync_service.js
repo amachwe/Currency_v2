4c78df6d65a31aa293d40a9eed263da8caf1967d
Azahar <amachwe@gmail.com>
2015-01-22 13:55:29 +0000

    Created bulk resync and service

--- bulk_resync_service.js
+++ bulk_resync_service.js
@@ -4,19 +4,19 @@ var fork = require("child_process").fork;
 const TIMER_DISABLED = 0;
 
 
-var timeInterval = tools.argv(process.argv[2],TIMER_DISABLED)*1;
+var timerInterval = tools.argv(process.argv[2],TIMER_DISABLED)*1;
 var TASK_RUNNING = false;
 
 if (timerInterval<0) {
     console.log("Timer interval (in minutes) must be greater than or equal to zero.");
 }
 
-if (timeInterval != TIMER_DISABLED) {
+if (timerInterval != TIMER_DISABLED) {
     setInterval(runTask,timerInterval*60*1000);
 }
 else
 {
-    console.log("TIMER has been disabled. Nothing to do and exiting.");
+    console.log("TIMER has been disabled. Nothing to do therefore exiting.");
 }
 
 
